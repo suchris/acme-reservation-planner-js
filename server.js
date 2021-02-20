e7c773bfcd01752e896659298a4e4544015632b7
Christine Su <christine.hui.su@gmail.com>
2021-02-20 13:31:28 -0500

    update index.js with render restaurants

--- server.js
+++ server.js
@@ -1,196 +1,214 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 const { STRING, ARRAY, FLOAT } = Sequelize;
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db');
-const volleyball = require('volleyball');
+const conn = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost/acme_db"
+);
+const volleyball = require("volleyball");
 
-const syncAndSeed = async()=> {
+const syncAndSeed = async () => {
   await conn.sync({ force: true });
 
   let restaurants = [
     {
       name: "Raos",
-      location: [-73.932, 40.7940]
+      location: [-73.932, 40.794],
     },
     {
       name: "Masa",
-      location: [-73.980, 40.7685]
+      location: [-73.98, 40.7685],
     },
     {
       name: "Bouley",
-      location: [-74.01394, 40.705137]
+      location: [-74.01394, 40.705137],
     },
     {
       name: "Marc Forgione",
-      location: [-74.009567, 40.716526]
+      location: [-74.009567, 40.716526],
     },
     {
       name: "Tamarind",
-      location: [-74.008929, 40.718977]
+      location: [-74.008929, 40.718977],
     },
     {
       name: "Hop Lee Restaurant",
-      location: [-73.998509, 40.71423]
+      location: [-73.998509, 40.71423],
     },
     {
       name: "Jungsik",
-      location: [-74.0089, 40.718679]
+      location: [-74.0089, 40.718679],
     },
     {
       name: "The Capital Grille",
-      location: [-74.010846, 40.708475]
+      location: [-74.010846, 40.708475],
     },
     {
       name: "Pylos",
-      location: [-73.984152, 40.726096]
+      location: [-73.984152, 40.726096],
     },
     {
       name: "Joe's Shanghai",
-      location: [-73.997761, 40.714601]
+      location: [-73.997761, 40.714601],
     },
     {
       name: "Cafe Katja",
-      location: [-73.990565, 40.717719]
+      location: [-73.990565, 40.717719],
     },
     {
       name: "Rosanjin",
-      location: [-74.007724, 40.716403]
+      location: [-74.007724, 40.716403],
     },
     {
       name: "Kittichai",
-      location: [-74.003242, 40.724014]
+      location: [-74.003242, 40.724014],
     },
     {
       name: "Bianca Restaurant",
-      location: [-73.992662, 40.725495]
+      location: [-73.992662, 40.725495],
     },
     {
       name: "Rayuela",
-      location: [-73.989756, 40.721266]
+      location: [-73.989756, 40.721266],
     },
     {
       name: "Mas Farmhouse",
-      location: [-74.003875, 40.729269]
+      location: [-74.003875, 40.729269],
     },
     {
       name: "Xe Lua",
-      location: [-73.998626, 40.716544]
-    }
+      location: [-73.998626, 40.716544],
+    },
   ];
 
-  restaurants = await Promise.all(restaurants.map( restaurant => Restaurant.create(restaurant)));
+  restaurants = await Promise.all(
+    restaurants.map((restaurant) => Restaurant.create(restaurant))
+  );
 
-  restaurants = restaurants.reduce( (acc, restaurant) => {
+  restaurants = restaurants.reduce((acc, restaurant) => {
     acc[restaurant.name] = restaurant;
     return acc;
-  }, {}); 
+  }, {});
 
-  let users = await Promise.all(['moe', 'lucy', 'larry'].map( name => User.create({ name })));
-  users = users.reduce( (acc, user) => {
+  let users = await Promise.all(
+    ["moe", "lucy", "larry"].map((name) => User.create({ name }))
+  );
+  users = users.reduce((acc, user) => {
     acc[user.name] = user;
     return acc;
   }, {});
 
   const reservations = await Promise.all([
-    Reservation.create({ userId: users.moe.id, restaurantId: restaurants.Tamarind.id}),
-    Reservation.create({ userId: users.lucy.id, restaurantId: restaurants.Tamarind.id}),
-    Reservation.create({ userId: users.lucy.id, restaurantId: restaurants.Rayuela.id})
+    Reservation.create({
+      userId: users.moe.id,
+      restaurantId: restaurants.Tamarind.id,
+    }),
+    Reservation.create({
+      userId: users.lucy.id,
+      restaurantId: restaurants.Tamarind.id,
+    }),
+    Reservation.create({
+      userId: users.lucy.id,
+      restaurantId: restaurants.Rayuela.id,
+    }),
   ]);
   return {
     users,
     restaurants,
-    reservations
+    reservations,
   };
-
 };
 
-const User = conn.define('user', {
+const User = conn.define("user", {
   name: {
-    type: STRING
-  }
+    type: STRING,
+  },
 });
-const Reservation = conn.define('reservation', {});
-const Restaurant = conn.define('restaurant', {
+const Reservation = conn.define("reservation", {});
+const Restaurant = conn.define("restaurant", {
   name: {
-    type: STRING
+    type: STRING,
   },
   location: {
     type: ARRAY(FLOAT),
-    defaultValue: []
-  }
+    defaultValue: [],
+  },
 });
 
 Reservation.belongsTo(User);
 Reservation.belongsTo(Restaurant);
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const chalk = require('chalk');
-const path = require('path');
+const chalk = require("chalk");
+const path = require("path");
 
 app.use(volleyball);
 
-app.use(require('body-parser').json());
+app.use(require("body-parser").json());
 
-app.use('/dist', express.static(path.join(__dirname, 'dist')));
+app.use("/dist", express.static(path.join(__dirname, "dist")));
 
-app.get('/', (req, res, next)=> res.sendFile(path.join(__dirname, 'index.html')));
+app.get("/", (req, res, next) =>
+  res.sendFile(path.join(__dirname, "index.html"))
+);
 
-app.get('/api/users', async(req, res, next)=> {
+app.get("/api/users", async (req, res, next) => {
   try {
     res.send(await User.findAll());
-  }
-  catch(ex){
+  } catch (ex) {
     next(ex);
   }
 });
 
-app.get('/api/restaurants', async(req, res, next)=> {
+app.get("/api/restaurants", async (req, res, next) => {
   try {
     res.send(await Restaurant.findAll());
-  }
-  catch(ex){
+  } catch (ex) {
     next(ex);
   }
 });
 
-app.get('/api/users/:userId/reservations', async(req, res, next)=> {
+app.get("/api/users/:userId/reservations", async (req, res, next) => {
   try {
-    res.send(await Reservation.findAll({ where: { userId: req.params.userId }}));
-  }
-  catch(ex){
+    res.send(
+      await Reservation.findAll({ where: { userId: req.params.userId } })
+    );
+  } catch (ex) {
     next(ex);
   }
 });
 
-app.post('/api/users/:userId/reservations', async(req, res, next)=> {
+app.post("/api/users/:userId/reservations", async (req, res, next) => {
   try {
-    res.status(201).send(await Reservation.create({ userId: req.params.userId, restaurantId: req.body.restaurantId}));
-  }
-  catch(ex){
+    res.status(201).send(
+      await Reservation.create({
+        userId: req.params.userId,
+        restaurantId: req.body.restaurantId,
+      })
+    );
+  } catch (ex) {
     next(ex);
   }
 });
 
-app.delete('/api/reservations/:id', async(req, res, next)=> {
+app.delete("/api/reservations/:id", async (req, res, next) => {
   try {
     const reservation = await Reservation.findByPk(req.params.id);
     await reservation.destroy();
     res.sendStatus(204);
-  }
-  catch(ex){
+  } catch (ex) {
     next(ex);
   }
 });
 
-app.use((err, req, res, next)=> {
+app.use((err, req, res, next) => {
   console.log(chalk.red(err.stack));
   res.status(500).send({ error: err.message });
 });
 const port = process.env.PORT || 3000;
 
-const init = async()=> {
+const init = async () => {
   await syncAndSeed();
-  app.listen(port, ()=> console.log(`listening on port ${port}`));
-}
+  app.listen(port, () => console.log(`listening on port ${port}`));
+};
 
 init();
--- src/index.js
+++ src/index.js
@@ -1,28 +1,48 @@
-import axios from 'axios';
+import axios from "axios";
 
-const userList = document.querySelector('#user-list');
-const restaurantList = document.querySelector('#restaurant-list');
-const reservationList = document.querySelector('#reservation-list');
+const userList = document.querySelector("#user-list");
+const restaurantList = document.querySelector("#restaurant-list");
+const reservationList = document.querySelector("#reservation-list");
 
 const renderUsers = (users) => {
-    const html = users.map(user => `
+  const html = users
+    .map(
+      (user) => `
     <li>
       <a href='#${user.id}'>
         ${user.name}
       </a>
+    </li>
+    `
+    )
+    .join("");
+  userList.innerHTML = html;
+};
+
+const renderRestaurants = (restaurants) => {
+  const html = restaurants
+    .map(
+      (restaurant) => `
     <li>
-    `).join('');
-    userList.innerHTML = html;
-}
+      <a href='#${restaurant.id}'>
+        ${restaurant.name}
+      </a>
+    </li>
+    `
+    )
+    .join("");
+  restaurantList.innerHTML = html;
+};
 
 const init = async () => {
-    try {
-        const users = (await axios.get('/api/users')).data;
-        renderUsers(users);
-    } catch (ex) {
-        console.log(ex);
-    }
-}
+  try {
+    const users = (await axios.get("/api/users")).data;
+    renderUsers(users);
+    const restaurants = (await axios.get("/api/restaurants")).data;
+    renderRestaurants(restaurants);
+  } catch (ex) {
+    console.log(ex);
+  }
+};
 
 init();
-
